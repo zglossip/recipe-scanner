c5088248561fadaf490c58a58781f92cc5b61872
Zach Glossip <zglossip@gmail.com>
2026-01-24 13:33:25 -0600

    Init

---
+++ src/main/java/com/zglossip/recipescanner/RecipeScannerApplication.java
@@ -0,0 +1,13 @@
+package com.zglossip.recipescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeScannerApplication.class, args);
+	}
+
+}
